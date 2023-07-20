661f908d5c246b61a31536c257a2f5e67de2a824
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-07-20 16:19:53 -0400

    added products route to app.js

--- client/app/App.js
+++ client/app/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
+import { Route, Routes } from "react-router-dom";
+import { AllProducts } from '../features/products/AllProducts';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <AppRoutes />
+      <Routes>
+        <Route path="/products" element={<AllProducts />} />
+      </Routes>
     </div>
   );
 };
