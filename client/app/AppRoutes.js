e16816a9ca3c3e6c327c23ea3eb54fc58e8c7635
Ivan Garcia <garcia.ivan1211@gmail.com>
2023-07-21 01:26:48 -0400

    Updated Products Back End, App.js and AppRoutes.js to try and display all products

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import AuthForm from '../features/auth/AuthForm';
 import Home from '../features/home/Home';
 import { me } from './store';
+import {AllProducts} from "../features/products/AllProducts"
 
 /**
  * COMPONENT
--- client/features/products/AllProducts.js
+++ client/features/products/AllProducts.js
@@ -35,4 +35,4 @@ export function AllProducts() {
   );
 }
 
-
+export default AllProducts;
