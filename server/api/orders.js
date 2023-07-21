84e8f3fff1e6a743dee1ca9409fb21976b916e35
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-07-21 16:29:16 -0400

    added order and orderProduct dummy data to database; updated orders API route

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-const {Orders} = require('../db/models')
+const {Orders, Users} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const orders = await Orders.findAll()
+    const orders = await Orders.findAll({
+        include: [Users], 
+      })
     res.json(orders)
   } catch (err) {
     next(err)
@@ -22,7 +24,7 @@ router.post("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
     try {
       const order = await Orders.findByPk(req.params.id);
-      res.json(task);
+      res.json(order);
     } catch (error) {
       next(error);
     }
